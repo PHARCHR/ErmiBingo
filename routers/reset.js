09a70839f44179d535afd36f45daafeb1fe2f1a3
PHARCHR <fargeta92@gmail.com>
2025-07-24 20:56:54 +0300

    fixing router

--- routers/reset.js
+++ routers/reset.js
@@ -8,9 +8,9 @@ const {
 } = require("../controllers/resetController");
 
 // Proper route definitions
-router.post("/", createReset);       // Create new reset password
-router.get("/", getReset);           // Get current reset password
-router.delete("/", deleteReset);     // Delete reset password
+router.post("/create", createReset);       // Create new reset password
+router.get("/get", getReset);           // Get current reset password
+router.delete("/delete", deleteReset);     // Delete reset password
 router.post("/validate", validateResetPassword);  // Add validation route
 
 module.exports = router;
